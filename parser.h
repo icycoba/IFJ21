a407c321e1e7c2e640dfd664e034f5cdc22d8454
icycoba <icycoba@gmail.com>
2021-11-20 13:18:08 +0100

    Funkce v hlavicce parseru pains

--- parser.h
+++ parser.h
@@ -26,3 +26,20 @@
 
 int parser();
 
+int syntax_program();
+int syntax_prolog();
+int syntax_fun_dec_def_call();
+int syntax_fun_call();
+int syntax_param_type();
+int syntax_param_type2();
+int syntax_type_rtrn();
+int syntax_type_rtrn2();
+int syntax_fun_params();
+int syntax_fun_params2();
+int syntax_stmts();
+int syntax_stmt();
+int syntax_var_init();
+int syntax_expr();
+int syntax_expr2();
+int syntax_type();
+int syntax_ID_next();
